57510c96d0fb2e176445ec1f7dc3c3d4c7523e8c
grmmahato <grmmahato12@gmail.com>
2024-03-26 17:19:47 +0545

    add student are fix

--- lib/util/apis.dart
+++ lib/util/apis.dart
@@ -1,10 +1,7 @@
 class Apis {
-
   static String baseApi = 'http://192.168.1.81:3000/api/v1';
   static String getCourseListApi = "$baseApi/course";
   static String getStudentApi = "$baseApi/student";
 
-  static String baseApi = 'http://192.168.1.81:3000';
-  static String postCourseApi = '${baseApi}/api/v1/course';
-
+  static String postCourseApi = '$baseApi/course';
 }
