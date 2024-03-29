2673e1af9de592e31a0ba08c92b80780b81a15e6
Sijan Pokharel <sijanpokharel@Sijans-MacBook-Pro.local>
2024-03-29 16:51:34 +0545

    update date picker

--- lib/util/custom_date_picker.dart
+++ lib/util/custom_date_picker.dart
@@ -34,7 +34,7 @@ class _CustomDatePickerState extends State<CustomDatePicker> {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: _selectedDate,
-      firstDate: DateTime(2015, 8),
+      firstDate: DateTime(1900),
       lastDate: DateTime(2101),
     );
     if (picked != null && picked != _selectedDate) {
